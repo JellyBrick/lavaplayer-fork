fcbe870ea21f217cd1238568e91f6908916acfd7
JellyBrick <shlee1503@naver.com>
2021-10-08 16:58:58 +0900

    fix: regex

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/container/mp3/Mp3FrameReader.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/container/mp3/Mp3FrameReader.java
@@ -89,7 +89,7 @@ public class Mp3FrameReader {
 
   private boolean parseFrameAt(int scanOffset) {
     if (scanOffset >= HEADER_SIZE && (frameSize = Mp3Decoder.getFrameSize(scanBuffer, scanOffset - HEADER_SIZE)) > 0) {
-      System.arraycopy(scanBuffer, scanOffset - 4, frameBuffer, 0, HEADER_SIZE);
+      System.arraycopy(scanBuffer, scanOffset - HEADER_SIZE, frameBuffer, 0, HEADER_SIZE);
 
       frameBufferPosition = HEADER_SIZE;
       return true;
