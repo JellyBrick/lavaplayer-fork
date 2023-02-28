c51fc4bd6e5c8847adc4b1e34c29121deb5cfce9
JellyBrick <shlee1503@naver.com>
2023-02-28 16:49:48 +0900

    bump deps

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -11,19 +11,19 @@ dependencies {
   api("com.sedmelluq:lava-common:1.1.2")
   implementation("com.github.walkyst:lavaplayer-natives-fork:1.0.0")
   implementation("com.github.walkyst.JAADec-fork:jaadec-ext-aac:0.1.3")
-  implementation("org.graalvm.js:js:22.0.0.2")
-  implementation("org.graalvm.js:js-scriptengine:22.0.0.2")
-  implementation("org.slf4j:slf4j-api:1.7.32")
+  implementation("org.graalvm.js:js:22.3.1")
+  implementation("org.graalvm.js:js-scriptengine:22.3.1")
+  implementation("org.slf4j:slf4j-api:1.7.36")
 
-  api("org.apache.httpcomponents:httpclient:4.5.13")
+  api("org.apache.httpcomponents:httpclient:4.5.14")
   implementation("commons-io:commons-io:2.11.0")
 
-  api("com.fasterxml.jackson.core:jackson-core:2.13.0")
-  api("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+  api("com.fasterxml.jackson.core:jackson-core:2.14.2")
+  api("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 
-  implementation("org.jsoup:jsoup:1.14.3")
+  implementation("org.jsoup:jsoup:1.15.4")
   implementation("net.iharder:base64:2.3.9")
-  implementation("org.json:json:20220924")
+  implementation("org.json:json:20230227")
 
   testImplementation("org.codehaus.groovy:groovy:2.5.15")
   testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
